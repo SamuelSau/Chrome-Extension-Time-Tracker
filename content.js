4dc2dae625d46d9e26c22668ac06ab3d7de17db0
Samuel Sau <samuel51@usf.edu>
2023-01-04 17:48:06 -0500

    specified sociam media webpages that extension run

--- content.js
+++ content.js
@@ -12,12 +12,14 @@ function trackTimeOnPage() {
 	// Record the time every second
 	let timeInterval = setInterval(function () {
 		let timeElapsed = Date.now() - timeStarted;
-		console.log('Time elapsed on page: ' + Math.floor(timeElapsed * 0.001) + 'seconds');
+		console.log(
+			`Time elapsed on page: ${Math.floor(timeElapsed * 0.001)} seconds`
+		);
 	}, 1000);
 
 	// Stop tracking time when the user leaves the page
 	window.onbeforeunload = function () {
 		clearInterval(timeInterval);
-		console.log('Total time spent on page: ' + (Date.now() - timeStarted));
+		console.log(`Total time spent on page: ${Date.now() - timeStarted}`);
 	};
 }
