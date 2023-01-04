ea3b4e02fa60c3f2d985a31dd8a9e8fc74668097
Samuel Sau <samuel51@usf.edu>
2023-01-04 17:35:21 -0500

    set time spent to seconds

--- content.js
+++ content.js
@@ -1,23 +1,23 @@
 // Listen for messages from the background script
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.action == "trackTime") {
-      trackTimeOnPage();
-    }
-  });
-  
-  // Track the time spent on the page
-  function trackTimeOnPage() {
-    let timeStarted = Date.now();
-  
-    // Record the time every second
-    let timeInterval = setInterval(function() {
-      let timeElapsed = Date.now() - timeStarted;
-      console.log("Time elapsed on page: " + timeElapsed);
-    }, 1000);
-  
-    // Stop tracking time when the user leaves the page
-    window.onbeforeunload = function() {
-      clearInterval(timeInterval);
-      console.log("Total time spent on page: " + (Date.now() - timeStarted));
-    };
-  }
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+	if (message.action == 'trackTime') {
+		trackTimeOnPage();
+	}
+});
+
+// Track the time spent on the page
+function trackTimeOnPage() {
+	let timeStarted = Date.now();
+
+	// Record the time every second
+	let timeInterval = setInterval(function () {
+		let timeElapsed = Date.now() - timeStarted;
+		console.log('Time elapsed on page: ' + Math.floor(timeElapsed * 0.001) + 'seconds');
+	}, 1000);
+
+	// Stop tracking time when the user leaves the page
+	window.onbeforeunload = function () {
+		clearInterval(timeInterval);
+		console.log('Total time spent on page: ' + (Date.now() - timeStarted));
+	};
+}
