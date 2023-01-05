dc2ba7928dd61cd5e2375dcd8c91e184425e55ac
Samuel Sau <samuel51@usf.edu>
2023-01-05 18:23:04 -0500

    removed redunancy in manifest and styled timerDiv

--- content.js
+++ content.js
@@ -5,10 +5,11 @@ timerDiv.id = 'timer';
 timerDiv.style.position = 'fixed';
 timerDiv.style.bottom = '10px';
 timerDiv.style.right = '10px';
-timerDiv.style.width = '200px';
-timerDiv.style.height = '100px';
-timerDiv.style.borderRadius = '20px';
-timerDiv.style.border = '2px solid black';
+timerDiv.style.width = '250px';
+timerDiv.style.height = '150px';
+timerDiv.style.borderRadius = '25px';
+timerDiv.style.border = '3px solid black';
+timerDiv.style.fontWeight = 'bold';
 timerDiv.style.backgroundColor = 'white';
 timerDiv.style.color = 'black';
 timerDiv.style.fontSize = '24px';
@@ -17,7 +18,23 @@ timerDiv.style.alignItems = 'center';
 timerDiv.style.justifyContent = 'center';
 document.body.appendChild(timerDiv);
 
+// Check if the elapsed time is stored in Chrome's storage
+chrome.storage.local.get(['totalElapsedTime'], (result) => {
+	// If the elapsed time is stored, use it as the starting value for the timer
+	if (result.totalElapsedTime) {
+		timer = result.totalElapsedTime;
+	}
+});
+
 setInterval(() => {
 	timer++;
-	document.getElementById('timer').innerHTML = timer;
+	const days = Math.floor(timer / 86400);
+	const hours = Math.floor((timer % 86400) / 3600);
+	const minutes = Math.floor(((timer % 86400) % 3600) / 60);
+	const seconds = ((timer % 86400) % 3600) % 60;
+	document.getElementById(
+		'timer'
+	).innerHTML = `${days}:${hours}:${minutes}:${seconds}`;
+	// Store the elapsed time in Chrome's storage
+	chrome.storage.local.set({ totalElapsedTime: timer });
 }, 1000);
