aa2aa563defa5f390bbd6bc8cd4b0b687eed20aa
Samuel Sau <samuel51@usf.edu>
2023-01-05 17:48:20 -0500

    simplified design by displaying timer on webpage

--- content.js
+++ content.js
@@ -1,29 +1,23 @@
-// Send a message to the service worker to get the elapsed time
-chrome.runtime.sendMessage({ type: 'get-elapsed-time' }, function (response) {
-	console.log('Time spent on website:', response.elapsedTime);
+let timer = 0;
 
-	// Create a new element to display the elapsed time
-	let timeElement = document.createElement('div');
-	timeElement.innerHTML = `${response.elapsedTime}`;
-	timeElement.style.position = 'fixed';
-	timeElement.style.bottom = '0';
-	timeElement.style.right = '0';
-	timeElement.style.backgroundColor = '#171717';
-	timeElement.style.color = '#F7FAFF2E';
-	timeElement.style.padding = '16px';
-	timeElement.style.color = 'white';
-	timeElement.style.padding = '10px';
-	timeElement.style.zIndex = '9999';
-	timeElement.style.height = '100px';
-	timeElement.style.width = '350px';
-	timeElement.style.borderRadius = '10%';
-	timeElement.style.fontSize = '24px';
-	timeElement.style.fontFamily = '-apple-system';
-	timeElement.style.textAlign = 'center';
-	timeElement.style.border = '2px solid white';
-	timeElement.style.display = 'flex';
-	timeElement.style.alignItems = 'center';
-	timeElement.style.justifyContent = 'center';
-	// Add the element to the body of the page
-	document.body.appendChild(timeElement);
-});
+const timerDiv = document.createElement('div');
+timerDiv.id = 'timer';
+timerDiv.style.position = 'fixed';
+timerDiv.style.bottom = '10px';
+timerDiv.style.right = '10px';
+timerDiv.style.width = '200px';
+timerDiv.style.height = '100px';
+timerDiv.style.borderRadius = '20px';
+timerDiv.style.border = '2px solid black';
+timerDiv.style.backgroundColor = 'white';
+timerDiv.style.color = 'black';
+timerDiv.style.fontSize = '24px';
+timerDiv.style.display = 'flex';
+timerDiv.style.alignItems = 'center';
+timerDiv.style.justifyContent = 'center';
+document.body.appendChild(timerDiv);
+
+setInterval(() => {
+	timer++;
+	document.getElementById('timer').innerHTML = timer;
+}, 1000);
