17908d27b313a7171999bc62ddd50d0f101ae914
Samuel Sau <samuel51@usf.edu>
2023-01-07 00:15:30 -0500

    removed subdomains and used URL api

--- popup.js
+++ popup.js
@@ -1,66 +1,60 @@
 let ctx = document.querySelector('canvas').getContext('2d');
-let timers = {};
+const timers = {
+	name: '',
+	timeSpent: 0,
+};
 
 //Query URL that our extension runs on and apply the timer value to the pie chart
 async function getResults() {
 	await chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+
 		const currentUrl = tabs[0].url;
 
 		//currentUrl contains the URL of the website that the extension is running on
 		chrome.storage.local.get([currentUrl], (result) => {
-			const subdomains = [
-				{ name: 'Youtube', regex: /^https?:\/\/[^.]*\.youtube\.com/ },
-				{ name: 'Facebook', regex: /^https?:\/\/[^.]*\.facebook\.com/ },
-				{ name: 'TikTok', regex: /^https?:\/\/[^.]*\.tiktok\.com/ },
-				{ name: 'WhatsApp', regex: /^https?:\/\/[^.]*\.whatsapp\.com/ },
-				{ name: 'Instagram', regex: /^https?:\/\/[^.]*\.instagram\.com/ },
-				{ name: 'Snapchat', regex: /^https?:\/\/[^.]*\.snapchat\.com/ },
-				{ name: 'Reddit', regex: /^https?:\/\/[^.]*\.reddit\.com/ },
-				{ name: 'Pinterest', regex: /^https?:\/\/[^.]*\.pinterest\.com/ },
-				{ name: 'Twitter', regex: /^https?:\/\/[^.]*\.twitter\.com/ },
-				{ name: 'Twitch', regex: /^https?:\/\/[^.]*\.twitch\.tv/ },
-				{ name: 'Discord', regex: /^https?:\/\/[^.]*\.discordapp\.com/ },
+			
+			const url = new URL(currentUrl);
+			const websiteName = url.hostname;
+
+			timers.name = websiteName;
+			timers['timeSpent'] = result[currentUrl];
+			
+			console.log(timers.name); //youtube.com
+			console.log(timers['timeSpent']); //2343
+			console.log(timers) //name: youtube.com, timeSpent: 2343
+
+			const results = [
+				{ name: 'Youtube', total: timers.Youtube || 50, shade: '#ff0000' },
+				{ name: 'Facebook', total: timers.Facebook || 50, shade: '#1877f2' },
+				{ name: 'TikTok', total: timers.TikTok || 50, shade: '#010101' },
+				{ name: 'WhatsApp', total: timers.WhatsApp || 50, shade: '#25d366' },
+				{ name: 'Instagram', total: timers.Instagram || 50, shade: '#c32aa3' },
+				{ name: 'Snapchat', total: timers.Snapchat || 50, shade: '#fffc00' },
+				{ name: 'Reddit', total: timers.Reddit || 50, shade: '#ff5700' },
+				{ name: 'Pinterest', total: timers.Pinterest || 50, shade: ' #bd081c' },
+				{ name: 'Twitter', total: timers.Twitter || 50, shade: '#1da1f2' },
+				{ name: 'Twitch', total: timers.Twitch ||50, shade: '#9146ff' },
+				{ name: 'Discord', total: timers.Discord || 50, shade: '#5865f2' },
 			];
 
-			const subdomain = subdomains.find(({ regex }) => currentUrl.match(regex));
-			const { name } = subdomain;
+			let sum = 0;
+			let totalNumberOfWebsites = results.reduce((sum, { total }) => sum + total, 0);
+			let currentAngle = 0;
 
-			timers[name] = result[currentUrl];
-
+			for (let name of results) {
+				//calculating the angle the slice (portion) will take in the chart
+				let portionAngle = (name.total / totalNumberOfWebsites) * 2 * Math.PI;
+				//drawing an arc and a line to the center to differentiate the slice from the rest
+				ctx.beginPath();
+				ctx.arc(100, 100, 100, currentAngle, currentAngle + portionAngle);
+				currentAngle += portionAngle;
+				ctx.lineTo(100, 100);
+				//filling the slices with the corresponding mood's color
+				ctx.fillStyle = name.shade;
+				ctx.fill();
+			}
 		});
 	});
 }
 getResults();
-//PiePart = {name: name, total: timer.Youtube, shade: '#394DFZ'}
-//append to results?
-const results = [
-	{ name: 'Youtube', total: timers.Youtube || 0, shade: '#960A2C' },
-	{ name: 'Facebook', total: timers.Facebook || 0, shade: '#332E2E' },
-	{ name: 'TikTok', total: timers.TikTok || 0, shade: '#F73809' },
-	{ name: 'WhatsApp', total: timers.WhatsApp || 0, shade: '#960A2C' },
-	{ name: 'Instagram', total: timers.Instagram || 0, shade: '#332E2E' },
-	{ name: 'Snapchat', total: timers.Snapchat || 0, shade: '#F73809' },
-	{ name: 'Reddit', total: timers.Reddit || 0, shade: '#960A2C' },
-	{ name: 'Pinterest', total: timers.Pinterest || 0, shade: '#332E2E' },
-	{ name: 'Twitter', total: timers.Twitter || 0, shade: '#F73809' },
-	{ name: 'Twitch', total: timers.Twitch || 0, shade: '#F73809' },
-	{ name: 'Discord', total: timers.Discord || 0, shade: '#F73809' },
-];
-console.log(results)
 
-let sum = 0;
-let totalNumberOfWebsites = results.reduce((sum, { total }) => sum + total, 0);
-let currentAngle = 0;
-
-for (let name of results) {
-	//calculating the angle the slice (portion) will take in the chart
-	let portionAngle = (name.total / totalNumberOfWebsites) * 2 * Math.PI;
-	//drawing an arc and a line to the center to differentiate the slice from the rest
-	ctx.beginPath();
-	ctx.arc(100, 100, 100, currentAngle, currentAngle + portionAngle);
-	currentAngle += portionAngle;
-	ctx.lineTo(100, 100);
-	//filling the slices with the corresponding mood's color
-	ctx.fillStyle = name.shade;
-	ctx.fill();
-}
